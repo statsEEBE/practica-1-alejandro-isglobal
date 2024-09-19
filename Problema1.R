cf9264d910d9e9477569d0854857b55bcb82e8a5
alejandro.caceres.dominguez <alejandro.caceres.dominguez@auleseebe.upc.edu>
2024-09-19 13:21:00 +0200

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,29 @@
 #Codigo para problema 1
-plot(1:10, col="green")
+
+mis_dades <- mtcars
+qseq_intervalos <- cut(mis_dades$qsec, 4)
+ni <- table(qseq_intervalos)
+fi <- ni/sum(ni)
+Ni <- cumsum(ni)
+Fi <- Ni/sum(ni)
+cbind(ni, fi, Ni, Fi)
+
+hist(mis_dades$qsec)
+mean(mis_dades$qsec)
+
+sort(mis_dades$drat)
+median(mis_dades$drat)
+
+quantile(mis_dades$drat, 0.25)
+quantile(mis_dades$drat, 0.75)
+
+quantile(mis_dades$mpg, 0.75)-quantile(mis_dades$mpg, 0.25)
+IQR(mis_dades$mpg)
+boxplot(mis_dades$mpg)
+
+sort(mis_dades$cyl)
+IQR(mis_dades$cyl)
+boxplot(mis_dades$cyl)
+
+sd(mis_dades$cyl)
+var(mis_dades$cyl)
